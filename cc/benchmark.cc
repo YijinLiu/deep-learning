5be4b1d423bfdf3c87ec5d86de042681b39cc25b
Chao Liu <yijinliu@gmail.com>
2017-07-31 23:26:58 -0700

    Make benchmark.cc work with OpenBLAS and MKL.

--- cc/benchmark.cc
+++ cc/benchmark.cc
@@ -32,7 +32,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-// 1. Linux i5-5575R 4.8.0-58-generic NumPy-1.13.1 with ATLAS-3.10.3
+// 1. Linux i5-5575R 4.8.0-58-generic Armadillo-7.950.1 with ATLAS-3.10.3
 // BM_MatrixMul/128       297072 ns     297068 ns       2367
 // BM_MatrixMul/256       885741 ns     885720 ns        779
 // BM_MatrixMul/512      6040530 ns    6039036 ns        113
@@ -45,4 +45,28 @@ int main(int argc, char** argv) {
 // BM_MatrixRank/1024  422724163 ns  422714866 ns          2
 // BM_MatrixRank/2048 3456366837 ns 3453005685 ns          1
 //
-// 2.
+// 2. Linux i5-5575R 4.8.0-58-generic Armadillo-7.950.1 with OpenBLAS-0.2.19
+// BM_MatrixMul/128        70116 ns      70115 ns       9573
+// BM_MatrixMul/256       458532 ns     458526 ns       1520
+// BM_MatrixMul/512      3522217 ns    3521357 ns        199
+// BM_MatrixMul/1024    26995594 ns   26995217 ns         26
+// BM_MatrixMul/2048   214196776 ns  214195269 ns          3
+//
+// BM_MatrixRank/128     1281774 ns    1276120 ns        545
+// BM_MatrixRank/256     5341550 ns    5341473 ns        125
+// BM_MatrixRank/512    29036400 ns   29036090 ns         24
+// BM_MatrixRank/1024  178252975 ns  178251867 ns          4
+// BM_MatrixRank/2048 1654905208 ns 1651834367 ns          1
+//
+// 3. Linux i5-5575R 4.8.0-58-generic Armadillo-7.950.1 with MKL-2017.3.196
+// BM_MatrixMul/128        14348 ns      14234 ns      50117
+// BM_MatrixMul/256       106776 ns     106726 ns       6314
+// BM_MatrixMul/512       874009 ns     867167 ns        817
+// BM_MatrixMul/1024     6764864 ns    6762785 ns        104
+// BM_MatrixMul/2048    52511307 ns   52171988 ns         12
+//
+// BM_MatrixRank/128      826153 ns     824132 ns        819
+// BM_MatrixRank/256     3630703 ns    3615387 ns        192
+// BM_MatrixRank/512    14279087 ns   14278752 ns         49
+// BM_MatrixRank/1024   73896350 ns   73894769 ns          9
+// BM_MatrixRank/2048  739607749 ns  739026617 ns          1
